430235fb4a4870a556344796d1f35ae88c1107b1
Dominick Baier <dbaier@leastprivilege.com>
2013-01-02 14:35:22 +0100

    Added RefreshToken repository

---
+++ src/Libraries/Thinktecture.IdentityServer.Core/Models/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Thinktecture.IdentityServer.Models
+{
+    public class RefreshToken
+    {
+        public string TokenIdentifier { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Scope { get; set; }
+    }
+}
---
+++ src/Libraries/Thinktecture.IdentityServer.Core/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license.txt
+ */
+
+using System.Collections.Generic;
+using System.Security.Claims;
+using Thinktecture.IdentityServer.Models;
+using Thinktecture.IdentityServer.TokenService;
+
+namespace Thinktecture.IdentityServer.Repositories
+{
+    /// <summary>
+    /// Repository for handling refresh tokens
+    /// </summary>
+    public interface IRefreshTokenRepository
+    {
+        string AddToken(string clientId, string userName, string scope);
+        RefreshToken GetToken(string tokenIdentifier);
+        void DeleteToken(string tokenIdentifier);
+    }
+}
